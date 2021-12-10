6f43533b2e3b704809227cd9b64ec27d1fce8bff
Ahmad <pouramini@gmail.com>
2021-12-10 18:56:56 +0330

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup, find_packages
+# version 
+setup(
+    name='comet',
+    version='1.0.0',
+    url='https://github.com/mypackage.git',
+    author='Author Name',
+    author_email='author@gmail.com',
+    description='Description of my package',
+    packages=find_packages(),    
+    #packages= ['comet','comet.evaluation'],
+    #package_dir={'': 'src'},
+    #install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
+)
